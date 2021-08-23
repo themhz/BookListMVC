6220290c7f1e35264d9b88ec989ea3dfddff8d4f
themhz <themhz@gmail.com>
2021-08-23 19:52:01 +0300

    Add project files.

---
+++ BookListMVC/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+        public String Author { get; set; }
+        public String ISBN { get; set; }
+
+
+
+    }
+}
